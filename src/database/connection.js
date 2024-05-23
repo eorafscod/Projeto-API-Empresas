0f8d39b3eefe4865fa4d8f3c96f9d69cced32eed
eorafscod <rafael.nobre.df@gmail.com>
2024-05-23 17:25:33 -0300

    adicionando departamento

--- src/database/connection.js
+++ src/database/connection.js
@@ -7,7 +7,7 @@ const DB_HOST = process.env.DB_HOST
 const DB_NAME = process.env.DB_NAME
 
 function main() {
-    mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`)
+    mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
         .then(() => console.log("Conectado ao banco Mongo!"))
         .catch(err => console.log("Erro ao conectar ao banco Mongo: ", err))
 }
